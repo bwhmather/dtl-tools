65c22ed147d4d5c27d3de5fded7e5c65e7c51dcc
Ben Mather <bwhmather@bwhmather.com>
2022-09-19 19:24:24 +0100

    Render something!

--- src/viewer.js
+++ src/viewer.js
@@ -1,4 +1,13 @@
 import { h, clobber } from "bdc";
+import {
+  from,
+  multicast,
+  Observable,
+  BehaviorSubject,
+  Subject,
+  combineLatest,
+  switchMap,
+} from "rxjs";
 import mainModuleUrl from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm";
 import mainWorkerUrl from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js";
 import * as duckdb from "@duckdb/duckdb-wasm";
@@ -67,17 +76,19 @@ class DTLSession {
    * snapshot.
    */
   async readSnapshotSchema(id) {
+    const snapshot = this.#manifest.snapshotById(id);
+
     let query = "DESCRIBE SELECT\n";
 
     // Add column bindings.
-    for (column of columns) {
+    for (const column of snapshot.columns()) {
       query += `    ${column.name}.values as ${column.name},\n`;
     }
 
     // Add source clauses.
     query += "FROM\n";
 
-    for (column of columns) {
+    for (const column of snapshot.columns()) {
       query += `    '${column.array}.parquet' ${column.name},\n`;
     }
 
@@ -86,24 +97,27 @@ class DTLSession {
     try {
       return await conn.query(query);
     } finally {
-      await con.close();
+      await conn.close();
     }
   }
 
   async readSnapshotLength(id) {
+    const snapshot = this.#manifest.snapshotById(id);
+    const columns = Array.from(snapshot.columns());
+
     let query = "SELECT\n";
 
     query += "    COUNT(*)\n";
 
     query += "FROM\n";
-    query += `    '${column.array}.parquet'\n`;
+    query += `    '${columns[0].array}.parquet'\n`;
 
     // Execute the query.
     const conn = await this.#db.connect();
     try {
       return await conn.query(query);
     } finally {
-      await con.close();
+      await conn.close();
     }
   }
 
@@ -112,33 +126,39 @@ class DTLSession {
    *
    * The schema should match the result from `readSnapshotSchema`.
    */
-  async readSnapshotData(id, { offset, limit }) {
+  async readSnapshotData(id, options) {
+    const snapshot = this.#manifest.snapshotById(id);
+    const columns = Array.from(snapshot.columns());
+
     let query = "SELECT\n";
 
     // Add column bindings.
-    for (column of columns) {
+    for (const column of columns) {
       query += `    ${column.name}.values as ${column.name},\n`;
     }
 
     // Add source clauses.
     query += "FROM\n";
 
-    for (column of columns) {
+    for (const column of columns) {
       query += `    '${column.array}.parquet' ${column.name},\n`;
     }
 
     // Add filter clauses.
-    if (columns.length > 1) {
+    /*    if (columns.length > 1) {
       query += "WHERE\n";
 
-      for (column of columns.slice(1)) {
-        query += `    ${column.name}.rowid = ${columns[0].name}.rowid,\n`;
+      const clauses = []
+      for (const column of columns.slice(1)) {
+        clauses.push(`    ${column.name}.rowid = ${columns[0].name}.rowid`);
       }
+      query += clauses.join(' AND\n');
+      query += '\n';
     }
-
+*/
     // Add order by clause.
-    query += "ORDER BY\n";
-    query += `    ${columns[0].name}.rowid\n`;
+    //  query += "ORDER BY\n";
+    //query += `    ${columns[0].name}.rowid\n`;
 
     // Add offset clause.
     if (typeof offset != "undefined") {
@@ -155,15 +175,151 @@ class DTLSession {
     try {
       return await conn.query(query);
     } finally {
-      await con.close();
+      await conn.close();
     }
   }
 }
 
+async function createSession(manifestUrl, arrayUrl) {
+  const session = new DTLSession(manifestUrl, arrayUrl);
+  await session.initialise();
+  return session;
+}
+
 export class DTLViewer extends HTMLElement {
+  #manifestUrl;
+  #arrayUrl;
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+
+    this.#manifestUrl = new Subject();
+    this.#arrayUrl = new Subject();
+
+    // Manifest + Data store => Session
+    const session = new Subject();
+    combineLatest({
+      manifestUrl: this.#manifestUrl,
+      arrayUrl: this.#arrayUrl,
+    })
+      .pipe(
+        switchMap(({ manifestUrl, arrayUrl }) => {
+          console.log({ manifestUrl, arrayUrl });
+          return from(
+            (async () => {
+              if (manifestUrl && arrayUrl) {
+                return await createSession(manifestUrl, arrayUrl);
+              }
+            })()
+          );
+        })
+      )
+      .subscribe(session);
+
+    // TODO
+    const snapshotId = new Observable((subscriber) => {
+      subscriber.next(0);
+    });
+
+    // Session + Target => Headers
+    const headers = new BehaviorSubject();
+
+    combineLatest({
+      session: session,
+      snapshotId: snapshotId,
+    })
+      .pipe(
+        switchMap(({ session, snapshotId }) =>
+          from(
+            (async () => {
+              if (!session) {
+                return;
+              }
+              return await session.readSnapshotSchema(snapshotId);
+            })()
+          )
+        )
+      )
+      .subscribe(headers);
+
+    // Session + Target => Length
+    const length = new BehaviorSubject();
+
+    combineLatest({
+      session: session,
+      snapshotId: snapshotId,
+    })
+      .pipe(
+        switchMap(({ session, snapshotId }) =>
+          from(
+            (async () => {
+              if (!session) {
+                return;
+              }
+              return await session.readSnapshotLength(snapshotId);
+            })()
+          )
+        )
+      )
+      .subscribe((length) => {
+        console.log(length);
+      });
+
+    // Session + Target + Scroll position => data
+    const data = new BehaviorSubject();
+
+    combineLatest({
+      session: session,
+      snapshotId: snapshotId,
+    })
+      .pipe(
+        switchMap(({ session, snapshotId }) =>
+          from(
+            (async () => {
+              if (!session) {
+                return;
+              }
+              return await session.readSnapshotData(snapshotId);
+            })()
+          )
+        )
+      )
+      .subscribe(data);
+
+    combineLatest({
+      headers,
+      length,
+      data,
+    }).subscribe(({ headers, length, data }) => {
+      if (!headers || !data) {
+        return;
+      }
+
+      const columnNames = [];
+      for (const column of headers) {
+        columnNames.push(column.column_name);
+      }
+
+      const rowElements = [];
+
+      const headerElements = [];
+      for (const column of headers) {
+        headerElements.push(h("th", column.column_name));
+      }
+      rowElements.push(h("tr", headerElements));
+
+      for (const row of data) {
+        rowElements.push(
+          h(
+            "tr",
+            columnNames.map((columnName) => h("td", "" + row[columnName]))
+          )
+        );
+      }
+
+      clobber(this.shadowRoot, h("table", rowElements));
+    });
   }
 
   renderSource() {
@@ -203,12 +359,13 @@ export class DTLViewer extends HTMLElement {
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name == "manifest" || name == "store") {
-      if (this.manifest && this.store) {
-        this.session = new DTLSession("./manifest.json", "./data");
-        this.session.initialise();
-        this.render();
-      }
+    switch (name) {
+      case "manifest":
+        this.#manifestUrl.next(newValue);
+        break;
+      case "store":
+        this.#arrayUrl.next(newValue);
+        break;
     }
   }
 
