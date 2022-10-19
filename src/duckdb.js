0efae8f8f38617b1560a9e25a9bdcbbed25f6e8d
Ben Mather <bwhmather@bwhmather.com>
2022-10-19 11:56:25 +0100

    License under GPL v3.0 or later

--- src/duckdb.js
+++ src/duckdb.js
@@ -1,3 +1,21 @@
+/**
+ * DTL Tools
+ * Copyright (C) 2022 Ben Mather <bwhmather@bwhmather.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import mainModuleUrl from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm";
 import mainWorkerUrl from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js";
 
